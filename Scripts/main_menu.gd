f9af7793f8260e2c43b6d3fffaef3f38a49e6ca3
Mateja <matejamarsenic@gmail.com>
2025-01-26 12:46:27 +0100

    main menu fix

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -5,7 +5,7 @@ extends Control
 func _on_play_pressed():
 	print("Play button pressed!")
 	# Change to the game scene
-	get_tree().change_scene("res://Scenes/Main.tscn")  # Replace with your game scene path
+	get_tree().change_scene_to_file("res://Scenes/Main.tscn")  # Replace with your game scene path
 
 
 
